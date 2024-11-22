aa81926314aaadcb5d506e8ae999cb5b5dd4c455
Ana Oliveira <anab.goncalo@gmail.com>
2024-11-21 22:20:13 -0300

    Correção de erro

---
+++ javascript/scApagarampos.js
@@ -0,0 +1,11 @@
+function limparInputs() {
+    // Seleciona todos os elementos de entrada no formulário
+    const inputs = document.querySelectorAll('input[type="number"]');
+    
+    // Itera por cada elemento e redefine o valor para vazio
+    inputs.forEach(input => {
+        input.value = "";
+    });
+
+    console.log("Todos os campos foram limpos.");
+}
